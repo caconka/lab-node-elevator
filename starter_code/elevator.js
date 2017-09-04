5e06062050d2c9902be898d8d28535426382b6ba
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-09-04 17:34:57 +0200

    done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,25 +1,69 @@
 class Elevator {
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
-    this.direction = "up";
+    this.floor       = 0
+    this.MAXFLOOR    = 10
+    this.direction   = "up"
+    this.requests    = []
+    this.waitingList = []
+    this.passengers  = []
   }
 
   start() {
     const interval = setInterval(() => this.update(), 1000)
   }
+
   stop() { 
-    clearInterval(interval)
+    setTimeout(() => clearInterval(interval), 5000)
   }
+
   update() { 
+    if(this.floor < this.requests[0]) {
+      this.direction = "up"
+      this._passengersLeave(); this._passengersEnter(); this.floorUp()
+    } else if(this.floor > this.requests[0]) {
+      this.direction = "down"
+      this._passengersLeave(); this._passengersEnter(); this.floorDown();
+    } else {
+      this._passengersLeave(); this._passengersEnter()
+    }
     this.log()
   }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
+
+  _passengersEnter() { 
+    for(let i=0; i<this.waitingList.length; i++) {
+      if(this.waitingList[i].originFloor == this.floor) {
+        this.passengers.push(this.waitingList[i])
+        this.requests.push(this.waitingList[i].destinationFloor)
+        console.log(`${this.waitingList[i].name} has enter the elevator`)
+        this.waitingList.splice(i,1)
+        this.requests.splice(i,1)
+      } 
+    }
+  }
+
+  _passengersLeave() { 
+    for(let i=0; i<this.passengers.length; i++) {
+      if(this.passengers[i].destinationFloor == this.floor) {
+        console.log(`${this.passengers[i].name} has left the elevator`)
+        this.passengers.splice(i,1)
+        this.requests.splice(i,1)
+      }
+    }
+  }
+
+  floorUp() { 
+    this.floor != this.MAXFLOOR ? this.floor ++ : console.log("Top floor")
+  }
+
+  floorDown() { 
+    this.floor != 0 ? this.floor -- : console.log("Ground floor")
+  }
+
+  call(person) { 
+    this.requests.push(person.originFloor)
+    this.waitingList.push(person)
+  }
+
   log() { 
     console.log(`Direction: ${this.direction} | Floor: ${this.floor}`)
   }
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,2 +1,9 @@
 const Elevator = require('./elevator.js')
 const Person = require('./person.js')
+
+let elevator = new Elevator()
+let julia = new Person("Julia", 3, 7)
+let juan = new Person("Juan", 2, 10)
+elevator.start()
+elevator.call(julia)
+setTimeout(()=> elevator.call(juan), 4000)
